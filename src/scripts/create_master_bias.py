9fa88eaff657306345f130e064b876de3119ca7a
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-09-21 16:37:32 +0200

    added master bias and updated plots

---
+++ src/scripts/create_master_bias.py
@@ -0,0 +1,30 @@
+import paths
+import utils
+import ccdproc as ccdp
+import matplotlib.pyplot as plt
+
+DIM = 1030
+raw_calib_path = paths.data / "raw_photometry" / "CALIB"
+save_path = paths.data / "processed_photometry" / "CALIB" / "bias"
+
+calib_collection = ccdp.ImageFileCollection(location=raw_calib_path)
+
+bias_filter = {
+    "object": "BIAS",
+    "naxis1": DIM,
+    "naxis2": DIM,
+}
+
+bias_collection = calib_collection.filter(**bias_filter)
+biases = list(bias_collection.ccds(ccd_kwargs={"unit": "adu"}))
+
+master_bias = utils.create_master(
+    biases, image_type="BIAS", save=True, save_path=save_path
+)
+
+fig, ax = plt.subplots(figsize=(10, 10))
+utils.show_image(master_bias, fig=fig, ax=ax, cbar_label="Signal [ADU]")
+ax.set_title("Master Bias", fontsize=20)
+ax.set_xlabel("X [pixels]", fontsize=16)
+ax.set_ylabel("Y [pixels]", fontsize=16)
+fig.tight_layout()
---
+++ src/scripts/random_bias_frames.py
@@ -0,0 +1,37 @@
+import paths
+import utils
+import numpy as np
+import ccdproc as ccdp
+import matplotlib.pyplot as plt
+
+DIM = 1030
+raw_calib_path = paths.data / "raw_photometry" / "CALIB"
+files = [
+    "EFOSC.2000-12-30T05:17:49.057.fits",
+    "EFOSC.2000-12-29T04:39:50.783.fits",
+    "EFOSC.2000-12-28T22:11:19.687.fits",
+    "EFOSC.2000-12-30T04:19:55.000.fits",
+]
+
+calib_collection = ccdp.ImageFileCollection(location=raw_calib_path, filenames=files)
+
+bias_filter = {
+    "object": "BIAS",
+    "naxis1": DIM,
+    "naxis2": DIM,
+}
+
+bias_collection = calib_collection.filter(**bias_filter)
+biases = list(bias_collection.ccds(ccd_kwargs={"unit": "adu"}))
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+ax = ax.flatten()
+for i, bias in enumerate(biases):
+    print(f"Mean bias {i + 1}: {np.mean(bias.data)}")
+    utils.show_image(bias, fig=fig, ax=ax[i], cbar_label="Signal [ADU]")
+    ax[i].set_title(f"Bias {i + 1}", fontsize=16)
+
+fig.suptitle("Randomly Selected Bias Frames", fontsize=20)
+fig.tight_layout()
+
+plt.savefig(paths.figures / "random_bias_frames.pdf")
