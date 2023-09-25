ba2856956993a440ea851fa259fd77adc2b22770
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-09-25 19:21:01 +0200

    Minor refactor

---
+++ src/scripts/master_bias_figure.py
@@ -0,0 +1,17 @@
+import paths
+import utils
+import ccdproc as ccdp
+import matplotlib.pyplot as plt
+
+master_bias = ccdp.CCDData.read(
+    paths.data / "processed_photometry" / "CALIB" / "bias" / "master_BIAS.fits",
+    unit="adu",
+)
+
+fig, ax = plt.subplots(figsize=(10, 10))
+utils.show_image(master_bias, fig=fig, ax=ax, cbar_label="Signal [ADU]")
+ax.set_title("Master Bias", fontsize=20)
+ax.set_xlabel("X [pixels]", fontsize=16)
+ax.set_ylabel("Y [pixels]", fontsize=16)
+fig.tight_layout()
+fig.savefig(paths.figures / "master_bias.pdf")
