17177ea265b777f0a78c642814375f8d6e921505
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-09-28 20:33:05 +0200

    minor fixes, swapped to 2d gauss centroid for stability

--- src/scripts/figures/bkg_included_standard_profiles.py
+++ src/scripts/figures/bkg_included_standard_profiles.py
@@ -22,109 +22,128 @@ init_centroids = (
 )
 
 flat_type = "sky"
-filter_name = "B"  # ["B", "V", "R"]
-colormap = cc.cm.kb
+filter_names = ["B", "V", "R"]
+colormaps = [cc.cm.kb, cc.cm.kg, cc.cm.kr]
 
-image_path = base_path / (filter_name + "_" + flat_type.lower() + "_image.fits")
-image = ccdp.CCDData.read(image_path)
+for filter_name, colormap in zip(filter_names, colormaps):
+    image_path = base_path / (filter_name + "_" + flat_type.lower() + "_image.fits")
+    image = ccdp.CCDData.read(image_path)
 
-fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
+    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
 
-for i, init_centroid in enumerate(init_centroids):
-    x0, y0 = init_centroid
-    xmax, xmin = x0 + DELTA, x0 - DELTA
-    ymax, ymin = y0 + DELTA, y0 - DELTA
+    for i, init_centroid in enumerate(init_centroids):
+        x0, y0 = init_centroid
+        xmax, xmin = x0 + DELTA, x0 - DELTA
+        ymax, ymin = y0 + DELTA, y0 - DELTA
 
-    trimmed_image = ccdp.trim_image(
-        image[ymin:ymax, xmin:xmax],
+        trimmed_image = ccdp.trim_image(
+            image[ymin:ymax, xmin:xmax],
+        )
+        data = trimmed_image.data
+        uncertainty = trimmed_image.uncertainty.array
+
+        xc, yc = cen.centroid_2dg(data, uncertainty)
+
+        show_colorbar = False
+        if i == 2:
+            show_colorbar = True
+
+        utils.show_image(
+            image,
+            ax=ax[0, i],
+            fig=fig,
+            cmap=colormap,
+            show_colorbar=show_colorbar,
+        )
+        print(f"Star {i+1} {filter_name}-centroid: ({xc:.2f}, {yc:.2f})")
+        ax[0, i].plot(
+            x0, y0, "x", color="gray", ms=10, mew=2, zorder=1000, label="Init. Centroid"
+        )
+        ax[0, i].plot(
+            xc + xmin,
+            yc + ymin,
+            "+",
+            color="white",
+            ms=10,
+            mew=2,
+            zorder=1000,
+            label="2D Gauss Centroid",
+        )
+        ax[0, i].legend(loc="upper right", fontsize=12)
+
+        profile = prof.RadialProfile(
+            data, (xc, yc), EDGE_RADII, error=uncertainty, mask=None, method="subpixel"
+        )
+
+        counts = profile.profile
+        mirrored_counts = np.concatenate((counts[::-1], counts))
+
+        uncertainty = profile.profile_error
+        mirrored_uncertainty = np.concatenate((uncertainty[::-1], uncertainty))
+
+        profile_radii = profile.radius
+        mirrored_radii = np.concatenate((-profile_radii[::-1], profile_radii))
+
+        fitter = mod.fitting.LevMarLSQFitter()
+        # Fit 1D Gauss
+        gauss_init = mod.models.Gaussian1D(
+            amplitude=np.nanmax(counts),
+            mean=0,
+            stddev=1,
+        )
+        gauss_fit = fitter(gauss_init, mirrored_radii, mirrored_counts)
+        std = gauss_fit.stddev.value
+        gauss_fwhm = 2 * np.sqrt(2 * np.log(2)) * std
+
+        # Fit 1D Moffat
+        moffat_init = mod.models.Moffat1D(
+            amplitude=np.nanmax(counts),
+            x_0=0,
+            gamma=1,
+            alpha=1,
+        )
+        moffat_fit = fitter(moffat_init, mirrored_radii, mirrored_counts)
+        alpha = moffat_fit.alpha.value
+        gamma = moffat_fit.gamma.value
+        moffat_fwhm = 2 * alpha * np.sqrt(2 ** (1 / gamma) - 1)
+
+        ax[1, i].errorbar(
+            profile_radii,
+            counts,
+            yerr=uncertainty,
+            fmt="o",
+            color="black",
+        )
+
+        ax[1, i].plot(
+            MOD_RADII,
+            gauss_fit(MOD_RADII),
+            color=utils.default_colors[0],
+            label=f"Gauss, FWHM = {gauss_fwhm:.2f}",
+            lw=2,
+        )
+        ax[1, i].plot(
+            MOD_RADII,
+            moffat_fit(MOD_RADII),
+            color=utils.default_colors[1],
+            label=f"Moffat, FWHM = {moffat_fwhm:.2f}",
+            lw=2,
+        )
+
+        ax[0, i].set_title(f"Star {i+1}", fontsize=14)
+        ax[0, i].set_xlim(xmin, xmax)
+        ax[0, i].set_ylim(ymin, ymax)
+        ax[0, i].set_xlabel("X [pixels]", fontsize=14)
+        if i == 0:
+            ax[0, i].set_ylabel("Y [pixels]", fontsize=14)
+            ax[1, i].set_ylabel("Electron Counts", fontsize=14)
+
+        ax[1, i].legend(loc="upper right", fontsize=12)
+
+        ax[1, i].set_xlabel("Radius [Pixels]", fontsize=14)
+
+    fig.tight_layout()
+    fig.savefig(
+        paths.figures / f"bkg_included_standard_{filter_name}_profiles.pdf",
+        bbox_inches="tight",
     )
-    data = trimmed_image.data
-    uncertainty = trimmed_image.uncertainty.array
-
-    xc, yc = cen.centroid_1dg(data, uncertainty)
-
-    show_colorbar = False
-    if i == 2:
-        show_colorbar = True
-
-    ax[0, i].set_title(f"Star {i+1}", fontsize=14)
-    utils.show_image(
-        image,
-        ax=ax[0, i],
-        fig=fig,
-        cmap=colormap,
-        show_colorbar=show_colorbar,
-    )
-    ax[0, i].set_xlim(xmin, xmax)
-    ax[0, i].set_ylim(ymin, ymax)
-    ax[0, i].set_xlabel("X [pixels]", fontsize=14)
-
-    profile = prof.RadialProfile(
-        data, (xc, yc), EDGE_RADII, error=uncertainty, mask=None, method="subpixel"
-    )
-
-    counts = profile.profile
-    mirrored_counts = np.concatenate((counts[::-1], counts))
-
-    uncertainty = profile.profile_error
-    mirrored_uncertainty = np.concatenate((uncertainty[::-1], uncertainty))
-
-    profile_radii = profile.radius
-    mirrored_radii = np.concatenate((-profile_radii[::-1], profile_radii))
-
-    fitter = mod.fitting.LevMarLSQFitter()
-    # Fit 1D Gauss
-    gauss_init = mod.models.Gaussian1D(
-        amplitude=np.nanmax(counts),
-        mean=0,
-        stddev=1,
-    )
-    gauss_fit = fitter(gauss_init, mirrored_radii, mirrored_counts)
-
-    # Fit 1D Moffat
-    moffat_init = mod.models.Moffat1D(
-        amplitude=np.nanmax(counts),
-        x_0=0,
-        gamma=1,
-        alpha=1,
-    )
-    moffat_fit = fitter(moffat_init, mirrored_radii, mirrored_counts)
-
-    ax[1, i].errorbar(
-        profile_radii,
-        counts,
-        yerr=uncertainty,
-        fmt="o",
-        color="black",
-    )
-    print("\nGauss")
-    print(gauss_fit(EDGE_RADII))
-    print("\nMoffat")
-    print(moffat_fit(EDGE_RADII))
-
-    ax[1, i].plot(
-        MOD_RADII,
-        gauss_fit(MOD_RADII),
-        color=utils.default_colors[0],
-        label="Gaussian",
-        lw=2,
-    )
-    ax[1, i].plot(
-        MOD_RADII,
-        moffat_fit(MOD_RADII),
-        color=utils.default_colors[1],
-        label="Moffat",
-        lw=2,
-    )
-    if i == 0:
-        ax[0, i].set_ylabel("Y [pixels]", fontsize=14)
-        ax[1, i].set_ylabel("Electron Counts", fontsize=14)
-        ax[1, i].legend(loc="upper right", fontsize=14)
-
-    ax[1, i].set_xlabel("Radius [Pixels]", fontsize=14)
-
-fig.tight_layout()
-fig.savefig(
-    paths.figures / "bkg_included_standard_profiles.pdf",
-    bbox_inches="tight",
-)
--- src/scripts/intermediate_results/standard_star_fwhm.py
+++ src/scripts/intermediate_results/standard_star_fwhm.py
@@ -22,77 +22,77 @@ init_centroids = (
 )
 
 flat_type = "sky"
-filter_name = "B"  # ["B", "V", "R"]
-colormap = cc.cm.kb
-
-image_path = base_path / (filter_name + "_" + flat_type.lower() + "_image.fits")
-image = ccdp.CCDData.read(image_path)
+filter_names = ["B", "V", "R"]
 
 output_path = paths.data / "processed_photometry" / "numbers" / "standard_stars_fwhm"
 
-for i, init_centroid in enumerate(init_centroids):
-    x0, y0 = init_centroid
-    xmax, xmin = x0 + DELTA, x0 - DELTA
-    ymax, ymin = y0 + DELTA, y0 - DELTA
+for filter_name in filter_names:
+    image_path = base_path / (filter_name + "_" + flat_type.lower() + "_image.fits")
+    image = ccdp.CCDData.read(image_path)
 
-    trimmed_image = ccdp.trim_image(
-        image[ymin:ymax, xmin:xmax],
-    )
-    data = trimmed_image.data
-    uncertainty = trimmed_image.uncertainty.array
+    for i, init_centroid in enumerate(init_centroids):
+        x0, y0 = init_centroid
+        xmax, xmin = x0 + DELTA, x0 - DELTA
+        ymax, ymin = y0 + DELTA, y0 - DELTA
 
-    xc, yc = cen.centroid_1dg(data, uncertainty)
+        trimmed_image = ccdp.trim_image(
+            image[ymin:ymax, xmin:xmax],
+        )
+        data = trimmed_image.data
+        uncertainty = trimmed_image.uncertainty.array
 
-    profile = prof.RadialProfile(
-        data, (xc, yc), EDGE_RADII, error=uncertainty, mask=None, method="subpixel"
-    )
+        xc, yc = cen.centroid_2dg(data, uncertainty)
 
-    counts = profile.profile
-    mirrored_counts = np.concatenate((counts[::-1], counts))
+        profile = prof.RadialProfile(
+            data, (xc, yc), EDGE_RADII, error=uncertainty, mask=None, method="subpixel"
+        )
 
-    uncertainty = profile.profile_error
-    mirrored_uncertainty = np.concatenate((uncertainty[::-1], uncertainty))
+        counts = profile.profile
+        mirrored_counts = np.concatenate((counts[::-1], counts))
 
-    profile_radii = profile.radius
-    mirrored_radii = np.concatenate((-profile_radii[::-1], profile_radii))
+        uncertainty = profile.profile_error
+        mirrored_uncertainty = np.concatenate((uncertainty[::-1], uncertainty))
 
-    fitter = mod.fitting.LevMarLSQFitter()
-    # Fit 1D Gauss
-    gauss_init = mod.models.Gaussian1D(
-        amplitude=np.nanmax(counts),
-        mean=0,
-        stddev=1,
-    )
-    gauss_fit = fitter(gauss_init, mirrored_radii, mirrored_counts)
-    std = gauss_fit.stddev.value
-    gauss_fwhm = 2 * np.sqrt(2 * np.log(2)) * std
+        profile_radii = profile.radius
+        mirrored_radii = np.concatenate((-profile_radii[::-1], profile_radii))
 
-    # Fit 1D Moffat
-    moffat_init = mod.models.Moffat1D(
-        amplitude=np.nanmax(counts),
-        x_0=0,
-        gamma=1,
-        alpha=1,
-    )
-    moffat_fit = fitter(moffat_init, mirrored_radii, mirrored_counts)
-    alpha = moffat_fit.alpha.value
-    gamma = moffat_fit.gamma.value
-    moffat_fwhm = 2 * alpha * np.sqrt(2 ** (1 / gamma) - 1)
+        fitter = mod.fitting.LevMarLSQFitter()
+        # Fit 1D Gauss
+        gauss_init = mod.models.Gaussian1D(
+            amplitude=np.nanmax(counts),
+            mean=0,
+            stddev=1,
+        )
+        gauss_fit = fitter(gauss_init, mirrored_radii, mirrored_counts)
+        std = gauss_fit.stddev.value
+        gauss_fwhm = 2 * np.sqrt(2 * np.log(2)) * std
 
-    values_arr = np.array(
-        [
-            [std, np.nan, gauss_fwhm],
-            [alpha, gamma, moffat_fwhm],
-        ]
-    )
-    col_names = ["par_1", "par_2", "FWHM"]
-    row_names = ["Gaussian", "Moffat"]
+        # Fit 1D Moffat
+        moffat_init = mod.models.Moffat1D(
+            amplitude=np.nanmax(counts),
+            x_0=0,
+            gamma=1,
+            alpha=1,
+        )
+        moffat_fit = fitter(moffat_init, mirrored_radii, mirrored_counts)
+        alpha = moffat_fit.alpha.value
+        gamma = moffat_fit.gamma.value
+        moffat_fwhm = 2 * alpha * np.sqrt(2 ** (1 / gamma) - 1)
 
-    utils.save_array_to_csv(
-        array=values_arr,
-        column_names=col_names,
-        index_names=row_names,
-        filename=f"standard_star_{i+1}_fwhm.csv",
-        path=output_path,
-        overwrite=True,
-    )
+        values_arr = np.array(
+            [
+                [std, np.nan, gauss_fwhm],
+                [alpha, gamma, moffat_fwhm],
+            ]
+        )
+        col_names = ["par_1", "par_2", "FWHM"]
+        row_names = ["Gaussian", "Moffat"]
+
+        utils.save_array_to_csv(
+            array=values_arr,
+            column_names=col_names,
+            index_names=row_names,
+            filename=f"standard_star_{filter_name}_{i+1}_fwhm.csv",
+            path=output_path,
+            overwrite=True,
+        )
